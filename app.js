47cbcb7b4fe1d0fbb8df8516c4aa8efe7ac83abf
SatishMiral <satishmiral01@gmail.com>
2024-10-26 00:06:40 +0530

    port settings

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get('/start-puppeteer', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 22;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on Port:${PORT}`);
 });
